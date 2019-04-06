75c61dc604ce5282a46bf81d4dfcd6b712c3c84c
Rick Henry <fredericmhenry@gmail.com>
2019-04-06 14:55:26 -0400

    what is going on
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- functions/submission-created/submission-created.js
+++ functions/submission-created/submission-created.js
@@ -1,10 +1,11 @@
 exports.handler = (event, context, callback) => {
   const body = JSON.parse(event.body)
-  // console.log(body)
-  console.log("email", body.data.email)
-  console.log("name", body.data.name)
-  console.log("choice", body.data.winner)
-  console.log("created", body.created_at)
+  console.log(body)
+  console.log(context)
+  // console.log("email", body.data.email)
+  // console.log("name", body.data.name)
+  // console.log("choice", body.data.winner)
+  // console.log("created", body.created_at)
   callback(null, {
     statusCode: 200,
     body: 'No worries, all is working fine'
