3101b325829be97cecb47bea42cac56f1e611464
Rick Henry <fredericmhenry@gmail.com>
2019-04-06 15:05:30 -0400

    unpack all data
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- functions/submission-created/submission-created.js
+++ functions/submission-created/submission-created.js
@@ -1,7 +1,9 @@
 exports.handler = (event, context, callback) => {
   const body = JSON.parse(event.body).payload
-  console.log(body)
+  // console.log(body)
   console.log("Email: ", body.data.email)
+  console.log("Name: ", body.data.name)
+  console.log("Winner: ", body.data.winner)
   callback(null, {
     statusCode: 200,
     body: 'No worries, all is working fine'
