2e4da0d3fcd53c916689b2b52f71c452c556ec88
Rick Henry <fredericmhenry@gmail.com>
2019-04-06 02:33:13 -0400

    this is so annoying
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,7 +1,7 @@
 exports.handler = (event, context, callback) => {
-  console.log(JSON.parse(event.body).payload)
+  console.log(JSON.parse(event.body).payload);
   callback(null, {
     statusCode: 200,
     body: 'No worries, all is working fine'
-  })
+  });
 }
