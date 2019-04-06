cb3676f9c247064ac2170ba9fd70e6cc57962daf
Rick Henry <fredericmhenry@gmail.com>
2019-04-06 02:30:27 -0400

    urgh
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,5 +1,5 @@
 exports.handler = (event, context, callback) => {
-  console.log(event.body)
+  console.log(JSON.parse(event.body).payload)
   callback(null, {
     statusCode: 200,
     body: 'No worries, all is working fine'
