cf7c0b1e0c7d5552c9f95370d2391dec34ae644e
Rick Henry <fredericmhenry@gmail.com>
2019-04-06 15:02:12 -0400

    add some extra logging and unpacking
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- functions/submission-created/submission-created.js
+++ functions/submission-created/submission-created.js
@@ -1,6 +1,7 @@
 exports.handler = (event, context, callback) => {
   const body = JSON.parse(event.body).payload
   console.log(body)
+  console.log("Email: ", body.data.email)
   callback(null, {
     statusCode: 200,
     body: 'No worries, all is working fine'
