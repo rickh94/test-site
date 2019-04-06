291bac1920da71ee3e60dc27b97ca8a8cf2b0b00
Rick Henry <fredericmhenry@gmail.com>
2019-04-06 15:42:40 -0400

    add airtable functionality
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- functions/submission-created/submission-created.js
+++ functions/submission-created/submission-created.js
@@ -1,11 +1,31 @@
+const Airtable = require('airtable')
+
+const apiKey = process.env.AIRTABLE_API_KEY
+const baseID = process.env.AIRTABLE_BASE_ID
+const tableName = process.env.TABLE_NAME
+
 exports.handler = (event, context, callback) => {
   const body = JSON.parse(event.body).payload
-  // console.log(body)
-  console.log("Email: ", body.data.email)
-  console.log("Name: ", body.data.name)
-  console.log("Winner: ", body.data.winner)
-  callback(null, {
-    statusCode: 200,
-    body: 'No worries, all is working fine'
-  });
+  const { email, name, winner } = body.data
+
+  const base = new Airtable({apiKey}).base(baseID)
+  base(tableName).create({
+    Email: email,
+    Name: name,
+    Choice: winner
+  }, function(err, record) {
+    if (err) {
+      console.error(err)
+      callback(null, {
+        statusCode: 422,
+        body: err
+      })
+      } else {
+      callback(null, {
+        statusCode: 200,
+        body: 'No worries, all is working fine'
+      })
+    }
+  })
+
 }
