e42f2219cd8cde2d3d5ceb38bd5a604cce89eee5
Rick Henry <fredericmhenry@gmail.com>
2019-04-06 21:01:04 -0400

    urgh
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- functions/submission-created/submission-created.js
+++ functions/submission-created/submission-created.js
@@ -1,21 +1,21 @@
 const Airtable = require('airtable')
-const firebase = require('firebase')
+// const firebase = require('firebase')
 
 const atApiKey = process.env.AIRTABLE_API_KEY
 const baseID = process.env.AIRTABLE_BASE_ID
 const tableName = process.env.TABLE_NAME
 
-const fbConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
-}
+// const fbConfig = {
+//   apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//   databaseURL: process.env.FIREBASE_DATABASE_URL,
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+// }
 
-const app = firebase.initializeApp(fbConfig)
-const db = firebase.database()
+// const app = firebase.initializeApp(fbConfig)
+// const db = firebase.database()
 
 exports.handler = (event, context, callback) => {
   const body = JSON.parse(event.body).payload
@@ -48,14 +48,14 @@ exports.handler = (event, context, callback) => {
     }
   })
 
-  const newResultKey = db.ref().child('results').push().key
-  db.ref(`/results/${newResultKey}`).set(newData, fbErr => {
-    if (err) {
-      err.push(fbErr)
-    } else {
-      return
-    }
-  })
+  // const newResultKey = db.ref().child('results').push().key
+  // db.ref(`/results/${newResultKey}`).set(newData, fbErr => {
+  //   if (err) {
+  //     err.push(fbErr)
+  //   } else {
+  //     return
+  //   }
+  // })
 
   if (err.length() !== 0) {
     console.log(err)
