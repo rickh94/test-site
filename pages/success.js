d01f8828b6efbd7ea0d580e7447f9c4a08ecf7c3
Rick Henry <fredericmhenry@gmail.com>
2019-04-07 02:35:40 -0400

    add progress bars to success page
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- pages/success.js
+++ pages/success.js
@@ -1,3 +1,31 @@
-import ProgressBar from 'progressbar'
+import ProgressBar from 'progressbar.js'
 
-line = new ProgressBar.Line('.result')
+const options = {
+  easing: 'easeIn',
+  text: {
+    style: {
+      color: '#999',
+      // position: 'relative',
+      // right: '0',
+      // top: '30px',
+      // padding: 0,
+      margin: '1vh 0 0 50vw',
+      transform: null
+    },
+    autoStyleContainer: false
+  },
+  color: '#ffb821',
+  step: (state, bar) => {
+    bar.setText(Math.round(bar.value() * 100) + '%')
+  }
+}
+
+const bar1 = new ProgressBar.Line('#choice1-votes', options)
+const bar2 = new ProgressBar.Line('#choice2-votes', options)
+const bar3 = new ProgressBar.Line('#choice3-votes', options)
+
+document.addEventListener('DOMContentLoaded', () => {
+  bar1.animate(0.2)
+  bar2.animate(0.6)
+  bar3.animate(0.2)
+})
