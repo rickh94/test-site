ffb94b436c5f79b47814a2bfaf7208e75f7bcd42
Rick Henry <fredericmhenry@gmail.com>
2019-04-08 00:09:33 -0400

    add dummy bios
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- functions/candidates.js
+++ functions/candidates.js
@@ -1,10 +1,27 @@
 const candidates = [
-    { name: 'Annmarie Akhbar' },
-    { name: 'Vivek Ingvar' },
-    { name: 'Kyriake Asil' },
-    { name: 'Jakob Victor' },
-    { name: 'Natille Tessa' },
-    { name: 'Anabela Bernardino' }
+    {
+        name: 'Annmarie Akhbar',
+        bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam, porro!'
+    },
+    {
+        name: 'Vivek Ingvar',
+        bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+    },
+    {
+        name: 'Kyriake Asil',
+        bio:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quibusdam modi explicabo aperiam, reiciendis saepe voluptas aut consequuntur excepturi! Saepe.'
+    },
+    {
+        name: 'Jakob Victor',
+        bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+    },
+    { name: 'Natille Tessa', bio: 'Lorem ipsum dolor sit.' },
+    {
+        name: 'Anabela Bernardino',
+        bio:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi laboriosam praesentium temporibus voluptates laudantium, quis id vero saepe!'
+    }
 ]
 
 exports.handler = function(event, context, callback) {
--- functions/submission-created/submission-created.js
+++ functions/submission-created/submission-created.js
@@ -6,7 +6,6 @@ exports.handler = (event, context, callback) => {
   const body = JSON.parse(event.body).payload
   const { email, name, winner } = body.data
   const dateCreated = new Date(body.created_at)
-  console.log(winner)
 
   let err = []
 
