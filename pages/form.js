4f9c2b5afd095f34caac44f1ca05487cd6e8c1f7
Rick Henry <fredericmhenry@gmail.com>
2019-08-06 11:24:59 -0400

    reformat
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- pages/form.js
+++ pages/form.js
@@ -1,47 +1,56 @@
 import { parse } from 'csv'
 document.addEventListener('DOMContentLoaded', () => {
-  const winnerDiv = document.getElementById('candidates')
-  // URL HARDCODE
+    const winnerDiv = document.getElementById('candidates')
+    // URL HARDCODE
     fetch('https://form.temptestsites.online/candidates.csv', {
-    mode: 'cors',
-    headers: new Headers({ 'Content-Type': 'application/json' })
-  })
-    .then(res => res.text())
-    .catch(err => {
-      document
-        .getElementById('error')
-        .appendChild(document.createTextNode(`Something has gone wrong: ${err}`))
+        mode: 'cors',
+        headers: new Headers({ 'Content-Type': 'application/json' }),
     })
-    .then(candidateData => {
-      parse(candidateData, { columns: true }, (err, candidates) => {
-        candidates.forEach((candidate, idx) => {
-          winnerDiv.innerHTML += `<p>
+        .then(res => res.text())
+        .catch(err => {
+            document
+                .getElementById('error')
+                .appendChild(
+                    document.createTextNode(`Something has gone wrong: ${err}`)
+                )
+        })
+        .then(candidateData => {
+            parse(candidateData, { columns: true }, (err, candidates) => {
+                candidates.forEach((candidate, idx) => {
+                    /* html */
+                    winnerDiv.innerHTML += `
+          <p>
         <div class="line">
-          <label for="candidate-${idx}" id="candidate-${idx}-label" class="label-container">${candidate.name}
-            <input type="radio" id="candidate-${idx}" name="winner" value="${candidate.name}">
+          <label for="candidate-${idx}" id="candidate-${idx}-label" class="label-container">${
+                        candidate.name
+                    }
+            <input type="radio" id="candidate-${idx}" name="winner" value="${
+                        candidate.name
+                    }">
             <span class="checkmark"></span>
           </label>
           <div class="downArrow" data-index="${idx}"></div>
         </div>
         <div id="bio-${idx}" style="display:none">${candidate.bio}</div>
-        </p>`
+        </p>
+        `
+                })
+                const arrows = document.querySelectorAll('.downArrow')
+                arrows.forEach(arrow => {
+                    arrow.addEventListener('click', e => {
+                        const idx = e.target.dataset.index
+                        const bio = document.getElementById(`bio-${idx}`)
+                        if (bio.style.display === 'none') {
+                            bio.style.display = 'block'
+                            e.target.classList.remove('downArrow')
+                            e.target.classList.add('upArrow')
+                        } else {
+                            bio.style.display = 'none'
+                            e.target.classList.remove('upArrow')
+                            e.target.classList.add('downArrow')
+                        }
+                    })
+                })
+            })
         })
-        const arrows = document.querySelectorAll('.downArrow')
-        arrows.forEach(arrow => {
-          arrow.addEventListener('click', e => {
-            const idx = e.target.dataset.index
-            const bio = document.getElementById(`bio-${idx}`)
-            if (bio.style.display === 'none') {
-              bio.style.display = 'block'
-              e.target.classList.remove('downArrow')
-              e.target.classList.add('upArrow')
-            } else {
-              bio.style.display = 'none'
-              e.target.classList.remove('upArrow')
-              e.target.classList.add('downArrow')
-            }
-          })
-        })
-      })
-    })
 })
